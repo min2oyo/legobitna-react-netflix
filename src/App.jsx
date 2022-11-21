e07cce6ee8aa4376ad8e39b574937a75f23a5480
min2oyo <min2u@outlook.com>
2022-11-21 16:22:27 +0900

    네비게이션바 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Movie from './pages/Movie';
 import MovieDetail from './pages/MovieDetail';
@@ -9,6 +11,7 @@ function App() {
   // 출력
   return (
     <>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movies' element={<Movie />} />
