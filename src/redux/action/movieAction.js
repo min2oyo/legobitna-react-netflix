b38dbfd0f6973e7c8066238363d3a60cbb4605b5
min2oyo <min2u@outlook.com>
2022-11-23 14:24:02 +0900

    movieReducer 적용

--- src/redux/action/movieAction.js
+++ src/redux/action/movieAction.js
@@ -7,13 +7,22 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 function getMovies() {
   return async dispatch => {
     const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-    const topRateApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
-    const upComingApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
+    const topRatedMovieApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
+    const upcomingMovieApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
 
-    let [popularMovies, topRateMovies, upComingMovies] = await Promise.all([popularMovieApi, topRateApi, upComingApi]);
+    let [popularMovies, topRatedMovies, upcomingMovies] = await Promise.all([popularMovieApi, topRatedMovieApi, upcomingMovieApi]);
     console.log('popularMovies: ', popularMovies);
-    console.log('topRateMovies: ', topRateMovies);
-    console.log('upComingMovies: ', upComingMovies);
+    console.log('topRatedMovies: ', topRatedMovies);
+    console.log('upcomingMovies: ', upcomingMovies);
+
+    dispatch({
+      type: "GET_MOVIES_SUCCESS",
+      payload: {
+        popularMovies: popularMovies.data,
+        topRatedMovies: topRatedMovies.data,
+        upcomingMovies: upcomingMovies.data,
+      }
+    });
   };
 }
 
--- src/redux/api.js
+++ src/redux/api.js
@@ -5,28 +5,29 @@ const api = axios.create({
   headers: { "Content-type": "application/json" },
 });
 
-// Add a request interceptor
-api.interceptors.request.use(function (config) {
-  // Do something before request is sent
-  console.log(`request OK: `, config);
-  return config;
-}, function (error) {
-  // Do something with request error
-  console.log(`request error: `, error);
-  return Promise.reject(error);
-});
+// request, response 확인
+// // Add a request interceptor
+// api.interceptors.request.use(function (config) {
+//   // Do something before request is sent
+//   console.log(`request OK: `, config);
+//   return config;
+// }, function (error) {
+//   // Do something with request error
+//   console.log(`request error: `, error);
+//   return Promise.reject(error);
+// });
 
-// Add a response interceptor
-api.interceptors.response.use(function (response) {
-  // Any status code that lie within the range of 2xx cause this function to trigger
-  // Do something with response data
-  console.log(`response OK: `, response);
-  return response;
-}, function (error) {
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
-  console.log(`response error: `, error);
-  return Promise.reject(error);
-});
+// // Add a response interceptor
+// api.interceptors.response.use(function (response) {
+//   // Any status code that lie within the range of 2xx cause this function to trigger
+//   // Do something with response data
+//   console.log(`response OK: `, response);
+//   return response;
+// }, function (error) {
+//   // Any status codes that falls outside the range of 2xx cause this function to trigger
+//   // Do something with response error
+//   console.log(`response error: `, error);
+//   return Promise.reject(error);
+// });
 
 export default api;
