196b8739bc49f23cf83287e0f86001b699f1069e
min2oyo <min2u@outlook.com>
2022-11-23 15:41:54 +0900

    배너 | 배경, 영화명, 설명 표시

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,21 @@
-const Banner = () => {
+const Banner = ({ movie }) => {
+
+  console.log(`movie:`, movie);
 
   // 출력
   return (
     <>
-      <div>Banner</div>
+      <div
+        className="banner"
+        style={{
+          backgroundImage: "url(" + `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie?.backdrop_path}` + ")",
+        }}
+      >
+        <div className="banner-info">
+          <h1>{movie?.title}</h1>
+          <p>{movie?.overview}</p>
+        </div>
+      </div>
     </>
   );
 };
