eb660782cefab0d6b631f76c408247108321c550
min2oyo <min2u@outlook.com>
2022-11-23 13:58:17 +0900

    API Promise 적용

--- src/redux/action/movieAction.js
+++ src/redux/action/movieAction.js
@@ -3,18 +3,17 @@ import api from "../api";
 // 변수
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-
 // 함수
 function getMovies() {
   return async dispatch => {
+    const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+    const topRateApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
+    const upComingApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
 
-    const popularMovieApi = await api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-
-    // let data = await (await axios(url)).json();
-
-    // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`;
-
-    // let url3 = `https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1`;
+    let [popularMovies, topRateMovies, upComingMovies] = await Promise.all([popularMovieApi, topRateApi, upComingApi]);
+    console.log('popularMovies: ', popularMovies);
+    console.log('topRateMovies: ', topRateMovies);
+    console.log('upComingMovies: ', upComingMovies);
   };
 }
 
