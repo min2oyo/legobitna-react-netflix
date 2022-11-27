7946be3dc4e42ef9ffb16153dfa84523d1a3465e
min2oyo <min2u@outlook.com>
2022-11-28 02:14:00 +0900

    슬라이드에 정보 출력

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,5 @@
+import { Badge } from "react-bootstrap";
+
 const MovieCard = ({ item }) => {
 
   // 출력
@@ -7,7 +9,18 @@ const MovieCard = ({ item }) => {
         className='card'
         style={{ backgroundImage: `url('https://www.themoviedb.org/t/p/w355_and_h200_multi_faces/${item.poster_path}')` }}
       >
-        MovieCard
+        <div className="overlay">
+          <h1>{item.title}</h1>
+          <div>
+            {item.genre_ids.map(id => (
+              <Badge key={id} bg="danger">{id}</Badge>
+            ))}
+          </div>
+          <div>
+            <span>{item.vote_average}</span>
+            <span>{item.adult ? `청불` : ``}</span>
+          </div>
+        </div>
       </div>
     </>
   );
